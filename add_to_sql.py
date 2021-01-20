297dcf85b50f8b40d15dbef47bc2aa3f561a36fb
prateekganguly1998 <gangulyprateek0@gmail.com>
2021-01-20 18:10:51 +0530

    day2

--- add_to_sql.py
+++ add_to_sql.py
@@ -4,11 +4,11 @@ from pathlib import Path
 from search_engine import DATABASE, PASSWORD, SERVER, USERNAME
 import json
 import mysql.connector 
+import pymysql.cursors
 import multiprocessing
 
-mydb = mysql.connector.connect (host='localhost',user='root',passwd='root',database='prateekb')
+mydb = pymysql.connect(host='localhost',user='root',passwd='root',database='prateekb', cursorclass=pymysql.cursors.DictCursor)
 base_path=Path(__file__)
-my_cursor=mydb.cursor()
 # db_path = app.config['SQLALCHEMY_DATABASE_URI']
 # e = create_engine(db_path)
 # s = Session(e)
@@ -24,9 +24,13 @@ def add_business():
     with open(path_to_json, encoding='utf-8') as f:
         business_data= json.load(f)
     resultTupList=list(map(convertdictToTuples,business_data))
-    sql = "INSERT INTO business ( business_id,name,address,city,state,postal_code,latitude,longitude,stars,review_count,is_open,attributes,categories,hours) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    my_cursor.executemany(sql,resultTupList)
-    mydb.commit()
+    with mydb:
+        with mydb.cursor() as cursor:
+            sql = "INSERT INTO business ( business_id,name,address,city,state,postal_code,latitude,longitude,stars,review_count,is_open,attributes,categories,hours) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            cursor.executemany(sql,resultTupList)
+            mydb.commit()
+    print('Added Business Data')
+    
 
 def add_tips():
     path_to_json = (base_path / "../yelp_academic_dataset_tip.json").resolve()
@@ -34,9 +38,13 @@ def add_tips():
         tips_data= json.load(f)
     resultTupList=list(map(convertdictToTuples,tips_data))
     print(resultTupList[0])
-    sql = "INSERT INTO tips ( user_id, business_id, text, date, compliment_count) VALUES (%s, %s, %s, %s, %s)"
-    my_cursor.executemany(sql,resultTupList)
-    mydb.commit()
+    with mydb:
+        with mydb.cursor() as cursor:
+             sql = "INSERT INTO tips ( user_id, business_id, text, date, compliment_count) VALUES (%s, %s, %s, %s, %s)"
+             cursor.executemany(sql,resultTupList)
+             mydb.commit()
+    print('Added Tips Data')
+   
 
 # def add_user():
 #     path_to_json = (base_path / "../yelp_academic_dataset_user.json").resolve()
